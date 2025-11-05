f997837d1953d3e4ca35681318982c44a7d476ef
rudresh <rudresh9nr@gmail.com>
2025-11-05 13:38:13 +0530

    first phase: Ship can fire 3 bullets/frame,ship can move left and right

--- src/settings.py
+++ src/settings.py
@@ -5,6 +5,13 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 600
         self.bg_color = (230, 230, 230)  # White background
-    
+
+        #Bullet settings
+        self.bullet_speed_factor = 0.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
         # Ship settings
         self.ship_speed_factor = 1.5
